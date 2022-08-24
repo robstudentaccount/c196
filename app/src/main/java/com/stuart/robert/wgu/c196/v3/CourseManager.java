d0dc46108bbbf4a7f51104cd7a6b37ee1f93195a
robstudentaccount <99552195+robstudentaccount@users.noreply.github.com>
2022-08-24 09:11:49 -0400

    Added Course name dialog and new theme

--- app/src/main/java/com/stuart/robert/wgu/c196/v3/CourseManager.java
+++ app/src/main/java/com/stuart/robert/wgu/c196/v3/CourseManager.java
@@ -1,7 +1,10 @@
 package com.stuart.robert.wgu.c196.v3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CourseManager extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class CourseManager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_manager);
+
+        Button addBtn = (Button) findViewById(R.id.addButton);
+
+        Intent courseIntent = new Intent(this, CourseName.class);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(courseIntent);
+            }
+        });
+
+
+
     }
 }
