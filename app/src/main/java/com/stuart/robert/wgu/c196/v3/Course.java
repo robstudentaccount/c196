ba816bb76ef7cef6351741fe02a571374f096087
robstudentaccount <99552195+robstudentaccount@users.noreply.github.com>
2022-09-02 08:18:32 -0400

    Added course start and end dates.

--- app/src/main/java/com/stuart/robert/wgu/c196/v3/Course.java
+++ app/src/main/java/com/stuart/robert/wgu/c196/v3/Course.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class Course implements Serializable {
     String name = "";
+    String startDate;
+    String endDate;
     public Course(String name) {
         this.name = name;
     }
@@ -11,8 +13,22 @@ public class Course implements Serializable {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
     }
 }
