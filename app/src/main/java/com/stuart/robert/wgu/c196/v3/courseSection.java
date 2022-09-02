db8b04d233b6ca6b2ca8464ee6f5e602a2b49f59
robstudentaccount <99552195+robstudentaccount@users.noreply.github.com>
2022-09-01 20:50:25 -0400

    Added start Date dialog

--- app/src/main/java/com/stuart/robert/wgu/c196/v3/courseSection.java
+++ app/src/main/java/com/stuart/robert/wgu/c196/v3/courseSection.java
@@ -1,18 +1,62 @@
 package com.stuart.robert.wgu.c196.v3;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class courseSection extends AppCompatActivity {
+
+    DatePickerDialog.OnDateSetListener startDate;
+    final Calendar myCalendarStart = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy", Locale.US);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_section);
         drawCourses();
+
+
+
+
+        TextView startDateTxt = (TextView) findViewById(R.id.startDateTxt);
+        startDateTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Date date;
+                String info = startDateTxt.getText().toString();
+                if(info.equals(""))info="03/22/22";
+                try {
+                    myCalendarStart.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                new DatePickerDialog(courseSection.this, startDate,
+                        myCalendarStart.get(Calendar.YEAR), myCalendarStart.get(Calendar.MONTH),
+                        myCalendarStart.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        startDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                myCalendarStart.set(Calendar.YEAR, year);
+                myCalendarStart.set(Calendar.MONTH, monthOfYear);
+                myCalendarStart.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                startDateTxt.setText(sdf.format(myCalendarStart.getTime()));
+            }
+        };
     }
     private void drawCourses() {
         LinearLayout courseListView = (LinearLayout) findViewById(R.id.courseListView);
