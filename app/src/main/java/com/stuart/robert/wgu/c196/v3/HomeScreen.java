12a329cd0411bb3d6e1cfdd3fac0a5317240092a
robstudentaccount <99552195+robstudentaccount@users.noreply.github.com>
2022-09-14 08:15:59 -0400

    Done

---
+++ app/src/main/java/com/stuart/robert/wgu/c196/v3/HomeScreen.java
@@ -0,0 +1,25 @@
+package com.stuart.robert.wgu.c196.v3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        Button vt = (Button) findViewById(R.id.hsVTbutton);
+        vt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
